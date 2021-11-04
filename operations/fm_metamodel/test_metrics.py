bc281bdfe9dc4a2299411cfc3cd441c7821c443c
José Miguel Horcas <jhorcas@us.es>
2021-11-04 18:26:14 +0100

    Add test for nof abstract and leaf features

--- operations/fm_metamodel/test_metrics.py
+++ operations/fm_metamodel/test_metrics.py
@@ -7,20 +7,28 @@ from famapy.metamodels.fm_metamodel.transformations.featureide_reader import Fea
 
 INPUT_MODELS_FOLDER = '../../models/featureide/'
 
-# filename, nof_features, nof_constraints, root_name, nof_ors, nof_alternatives, nof_core_features
-MODELS = [['pizzas', 12, 1, 'Pizza', 1, 2, 4],
-          ['wget', 17, 0, 'wget', 0, 1, 2],
-          ['tankwar', 37, 0, 'TankWar', 2, 6, 7],
-          ['mobilemedia2', 43, 3, 'MobileMedia2', 4, 3, 10],
-          ['jHipster', 45, 13, 'JHipster', 0, 10, 0],
-          ['WeaFQAs', 179, 7, 'FQAs', 13, 23, 1],
-          ['busybox-1.18.0', 854, 67, 'root', 0, 8, 20],
-          ['embtoolkit', 1179, 167, 'root', 0, 70, 78],
-          ['ea2468', 1408, 1281, 'root', 0, 12, 4],
-          ['uClinux-distribution', 1580, 247, 'root', 0, 10, 6],
-          ['automotive2_1', 14010, 624, 'N_379925076__F_91527E35945B', 106, 1010, 1394],
-          ['linux-2.6.33.3', 6467, 7650, 'root', 2, 39, 53]]
-          
+# 0 filename
+# 1 nof features 
+# 2 nof constraints
+# 3 root name
+# 4 nof ors
+# 5 nof alternatives
+# 6 nof abstract features
+# 7 nof leaf features
+# 8 nof core features
+MODELS = [['pizzas', 12, 1, 'Pizza', 1, 2, 1, 8, 4],
+          ['wget', 17, 0, 'wget', 0, 1, 0, 15, 2],
+          ['tankwar', 37, 0, 'TankWar', 2, 6, 0, 26, 7],
+          ['mobilemedia2', 43, 3, 'MobileMedia2', 4, 3, 0, 31, 10],
+          ['jHipster', 45, 13, 'JHipster', 0, 10, 13, 32, 0],
+          ['WeaFQAs', 179, 7, 'FQAs', 13, 23, 0, 124, 1],
+          ['busybox-1.18.0', 854, 67, 'root', 0, 8, 0, 683, 20],
+          ['embtoolkit', 1179, 167, 'root', 0, 70, 0, 966, 78],
+          ['ea2468', 1408, 1281, 'root', 0, 12, 0, 1069, 4],
+          ['uClinux-distribution', 1580, 247, 'root', 0, 10, 0, 1368, 6],
+          ['automotive2_1', 14010, 624, 'N_379925076__F_91527E35945B', 106, 1010, 0, 12615, 1394],
+          ['linux-2.6.33.3', 6467, 7650, 'root', 2, 39, 0, 5523, 53]]
+
 
 class FMMetricsTest(unittest.TestCase):
 
@@ -62,6 +70,21 @@ class FMMetricsTest(unittest.TestCase):
         fm = reader.transform()
         result_alternative_groups = sum(map(lambda f: f.is_alternative_group(), fm.get_features()))
         self.assertEqual(result_alternative_groups, nof_alternative_groups)
+
+    @parameterized.expand([[m[0], m[6]] for m in MODELS])
+    def test_nof_abstract_features(self, fm_input, nof_abstract_features):
+        reader = self.reader(self.input_folder + fm_input + self.ext)
+        fm = reader.transform()
+        result_abstract_features = sum(map(lambda f: f.is_abstract, fm.get_features()))
+        self.assertEqual(result_abstract_features, nof_abstract_features)
+    
+    @parameterized.expand([[m[0], m[7]] for m in MODELS])
+    def test_nof_leaf_features(self, fm_input, nof_leaf_features):
+        reader = self.reader(self.input_folder + fm_input + self.ext)
+        fm = reader.transform()
+        result_leaf_features = sum(map(lambda f: f.is_leaf(), fm.get_features()))
+        self.assertEqual(result_leaf_features, nof_leaf_features)
+
 
 if __name__ == '__main__':
     unittest.main()
