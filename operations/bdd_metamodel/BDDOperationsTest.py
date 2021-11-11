53c7944d92ad29b9a4a8926ab8cc2b67248493ae
José Miguel Horcas <jhorcas@us.es>
2021-11-11 16:37:58 +0100

    Refactor info for real feature models

--- operations/bdd_metamodel/BDDOperationsTest.py
+++ operations/bdd_metamodel/BDDOperationsTest.py
@@ -7,41 +7,21 @@ from famapy.metamodels.bdd_metamodel.operations import (
     BDDProductDistributionBF
 )
 
-# INDEX: ATTRIBUTE
-# 0 filename
-# 1 nof of products
-# product distribution
-MODELS = [('pizzas', 42, [0, 0, 0, 0, 0, 0, 0, 12, 18, 10, 2, 0, 0]),
-          ('wget', 8192, [0, 0, 1, 11, 58, 198, 495, 957, 1452, 1716, 1551, 1045, 506, 166, 33, 3, 0, 0]),
-          ('GPL', 436, [0, 0, 0, 0, 0, 0, 0, 1, 10, 27, 52, 73, 83, 78, 61, 36, 13, 2, 0]),
-          ('tankwar', 1741824, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 72, 216, 1176, 4008, 12072, 30792, 65856, 120840, 189792, 254880, 291888, 282456, 227088, 147864, 75288, 28608, 7584, 1248, 96, 0, 0, 0, 0, 0, 0, 0]),
-          ('mobilemedia2', 3096576, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 72, 684, 3012, 8736, 20376, 43380, 86736, 156864, 247872, 339732, 408252, 436416, 417852, 355992, 264852, 167604, 87468, 36300, 11436, 2556, 360, 24, 0, 0, 0, 0, 0, 0]),
-          ('jHipster', 26256, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 8, 16, 42, 106, 200, 238, 250, 488, 1276, 2688, 4314, 5460, 5322, 3696, 1668, 432, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-          ('aafms_framework-namesAdapted', 132224790560, []),
-          ('WeaFQAs', 1677039951397603738337280, []),
-          ('busybox-1.18.0', 0, []),
-          ('embtoolkit', 0, []),
-          ('ea2468', 0, []),
-          ('uClinux-distribution', 0, []),
-          ('automotive2_1', 0, []),
-          ('linux-2.6.33.3', 0, [])]
-
-INPUT_MODELS_FOLDER = 'models/featureide/'
-EXTESION = '.xml'
+from models.models_info import *
 
 
 def get_model(model_name):
-    fm = FeatureIDEReader(INPUT_MODELS_FOLDER + model_name + EXTESION).transform()
+    fm = FeatureIDEReader(INPUT_MODELS_FOLDER + model_name + EXTENSION).transform()
     bdd = FmToBDD(fm).transform()
     return bdd
 
-@pytest.mark.parametrize("model_name, expected_nof_products", [[m[0], m[1]] for m in MODELS[:8]])
+@pytest.mark.parametrize("model_name, expected_nof_products", [[m[NAME], m[NOF_PRODUCTS]] for m in MODELS[:8]])
 def test_nof_products(model_name, expected_nof_products):
     model = get_model(model_name)
     nof_products = BDDProductsNumber().execute(model).get_result()
     assert nof_products == expected_nof_products
 
-@pytest.mark.parametrize("model_name, expected_product_dist", [[m[0], m[2]] for m in MODELS[:6]])
+@pytest.mark.parametrize("model_name, expected_product_dist", [[m[NAME], m[PRODUCT_DISTRIBUTION]] for m in MODELS[:6]])
 def test_product_distribution(model_name, expected_product_dist):
     model = get_model(model_name)
     product_dist = BDDProductDistributionBF().execute(model).get_result()
--- operations/fm_metamodel/FMOperationsTest.py
+++ operations/fm_metamodel/FMOperationsTest.py
@@ -7,49 +7,26 @@ from famapy.metamodels.fm_metamodel.operations import (
     FMEstimatedProductsNumber
 )
 
-# INDEX: ATTRIBUTE
-# 0 filename
-# 1 nof core features
-# 2 nof dead features
-# 3 nof false-optional features
-# 4 nof leaf features
-# 5 nof of products
-MODELS = [('pizzas', 4, 0, 0, 8, 42),
-          ('wget', 2, 0, 0, 15, 8192),
-          ('GPL', 5, 0, 0, 13, 436),
-          ('tankwar', 7, 0, 0, 26, 1741824),
-          ('mobilemedia2', 10, 0, 0, 31, 3096576),
-          ('jHipster', 7, 0, 0, 32, 26256),
-          ('aafms_framework-namesAdapted', 3, 0, 0, 45, 132224790560),
-          ('WeaFQAs', 1, 0, 0, 124, 1677039951397603738337280),
-          ('busybox-1.18.0', 20, 15, 3, 683, 0),
-          ('embtoolkit', 78, 42, 0, 966, 0),
-          ('ea2468', 4, 124, 222, 1069, 0),
-          ('uClinux-distribution', 6, 0, 0, 1368, 0),
-          ('automotive2_1', 1394, 1, 2, 12615, 0),
-          ('linux-2.6.33.3', 53, 211, 39, 5523, 0)]
-
-INPUT_MODELS_FOLDER = 'models/featureide/'
-EXTESION = '.xml'
+from models.models_info import *
 
 
 def get_model(model_name) -> str:
-    return FeatureIDEReader(INPUT_MODELS_FOLDER + model_name + EXTESION).transform()
+    return FeatureIDEReader(INPUT_MODELS_FOLDER + model_name + EXTENSION).transform()
 
 
-@pytest.mark.parametrize("model_name, expected_nof_core_features", [m[:2] for m in MODELS])
+@pytest.mark.parametrize("model_name, expected_nof_core_features", [[m[NAME], m[NOF_CORE_FEATURES]] for m in MODELS])
 def test_nof_core_features(model_name, expected_nof_core_features):
     fm = get_model(model_name)
     core_features = FMCoreFeatures().execute(fm).get_result()
     assert len(core_features) == expected_nof_core_features
 
-@pytest.mark.parametrize("model_name, expected_nof_leaf_features", [[m[0], m[4]] for m in MODELS])
+@pytest.mark.parametrize("model_name, expected_nof_leaf_features", [[m[NAME], m[NOF_LEAF_FEATURES]] for m in MODELS])
 def test_nof_leaf_features(model_name, expected_nof_leaf_features):
     fm = get_model(model_name)
     leaf_features = FMCountLeafs().execute(fm).get_result()
     assert leaf_features == expected_nof_leaf_features
 
-@pytest.mark.parametrize("model_name, expected_nof_products", [[m[0], m[5]] for m in MODELS])
+@pytest.mark.parametrize("model_name, expected_nof_products", [[m[NAME], m[NOF_PRODUCTS]] for m in MODELS])
 def test_nof_products(model_name, expected_nof_products):
     fm = get_model(model_name)
     nof_products = FMEstimatedProductsNumber().execute(fm).get_result()
