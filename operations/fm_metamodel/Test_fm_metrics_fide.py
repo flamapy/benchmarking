f79c9be21162884e661d45f68fcb283dc45b87ed
José Miguel Horcas <jhorcas@us.es>
2021-11-12 14:11:50 +0100

    Refactor tests for fm and pysat

---
+++ operations/fm_metamodel/Test_fm_metrics_fide.py
@@ -0,0 +1,50 @@
+import pytest
+
+from famapy.metamodels.fm_metamodel.transformations.featureide_reader import FeatureIDEReader
+
+from models.models_info import *
+
+
+def get_model(model_name) -> str:
+    return FeatureIDEReader(INPUT_FIDE_MODELS_FOLDER + model_name + FIDE_EXTENSION).transform()
+
+
+@pytest.mark.parametrize("model_name, expected_nof_features", [[m[NAME], m[NOF_FEATURES]] for m in MODELS])
+def test_nof_features(model_name, expected_nof_features):
+    fm = get_model(model_name)
+    assert len(fm.get_features()) == expected_nof_features
+
+@pytest.mark.parametrize("model_name, expected_nof_constraints", [[m[NAME], m[NOF_CONSTRAINTS]] for m in MODELS])
+def test_nof_constraints(model_name, expected_nof_constraints):
+    fm = get_model(model_name)
+    assert len(fm.get_constraints()) == expected_nof_constraints
+
+@pytest.mark.parametrize("model_name, expected_root_feature", [[m[NAME], m[ROOT_NAME]] for m in MODELS])
+def test_root_feature(model_name, expected_root_feature):
+    fm = get_model(model_name)
+    assert fm.root.name == expected_root_feature
+    assert fm.root.get_parent() is None
+
+@pytest.mark.parametrize("model_name, expected_nof_or_groups", [[m[NAME], m[NOF_ORS]] for m in MODELS])    
+def test_nof_or_groups(model_name, expected_nof_or_groups):
+    fm = get_model(model_name)
+    result_or_groups = sum(map(lambda f: f.is_or_group(), fm.get_features()))
+    assert result_or_groups == expected_nof_or_groups
+
+@pytest.mark.parametrize("model_name, expected_nof_xor_groups", [[m[NAME], m[NOF_ALTERNATIVES]] for m in MODELS])
+def test_nof_alternative_groups(model_name, expected_nof_xor_groups):
+    fm = get_model(model_name)
+    result_alternative_groups = sum(map(lambda f: f.is_alternative_group(), fm.get_features()))
+    assert result_alternative_groups == expected_nof_xor_groups
+
+@pytest.mark.parametrize("model_name, expected_nof_abstract_feats", [[m[NAME], m[NOF_ABSTRACT_FEATURES]] for m in MODELS])
+def test_nof_abstract_features(model_name, expected_nof_abstract_feats):
+    fm = get_model(model_name)
+    result_abstract_features = sum(map(lambda f: f.is_abstract, fm.get_features()))
+    assert result_abstract_features == expected_nof_abstract_feats
+
+@pytest.mark.parametrize("fm_input, expected_nof_leaf_features", [[m[NAME], m[NOF_LEAF_FEATURES]] for m in MODELS])
+def test_nof_leaf_features(fm_input, expected_nof_leaf_features):
+    fm = get_model(fm_input)
+    result_leaf_features = sum(map(lambda f: f.is_leaf(), fm.get_features()))
+    assert result_leaf_features == expected_nof_leaf_features
---
+++ operations/pysat_metamodel/Test_pysat_fide.py
@@ -0,0 +1,51 @@
+from famapy.metamodels.pysat_metamodel.models.pysat_model import PySATModel
+import pytest
+
+from famapy.metamodels.fm_metamodel.models import FeatureModel
+from famapy.metamodels.fm_metamodel.transformations.featureide_reader import FeatureIDEReader
+from famapy.metamodels.pysat_metamodel.transformations.fm_to_pysat import FmToPysat
+
+from famapy.metamodels.pysat_metamodel.operations.glucose3_products_number import Glucose3ProductsNumber
+from famapy.metamodels.pysat_metamodel.operations.glucose3_valid import Glucose3Valid
+from famapy.metamodels.pysat_metamodel.operations.glucose3_core_features import Glucose3CoreFeatures
+from famapy.metamodels.pysat_metamodel.operations.glucose3_dead_features import Glucose3DeadFeatures
+from famapy.metamodels.pysat_metamodel.operations.glucose3_false_optional_features import Glucose3FalseOptionalFeatures
+
+from models.models_info import *
+
+
+def get_model(model_name: str) -> tuple[FeatureModel, PySATModel]:
+    fm = FeatureIDEReader(INPUT_FIDE_MODELS_FOLDER + model_name + FIDE_EXTENSION).transform()
+    pysat_model = FmToPysat(fm).transform()
+    return (fm, pysat_model)
+
+@pytest.mark.parametrize("model_name, expected_nof_products", [[m[NAME], m[NOF_PRODUCTS]] for m in MODELS[:4]])
+def test_nof_products(model_name, expected_nof_products):
+    _, model = get_model(model_name)
+    nof_products = Glucose3ProductsNumber().execute(model).get_result()
+    assert nof_products == expected_nof_products
+
+@pytest.mark.parametrize("model_name", [m[0] for m in MODELS])
+def test_valid_model(model_name):
+    _, model = get_model(model_name)
+    valid = Glucose3Valid().execute(model).get_result()
+    assert valid
+
+@pytest.mark.parametrize("model_name, expected_nof_core_features", [[m[NAME], m[NOF_CORE_FEATURES]] for m in MODELS])
+def test_nof_core_features(model_name, expected_nof_core_features):
+    _, model = get_model(model_name)
+    core_features = Glucose3CoreFeatures().execute(model).get_result()
+    assert len(core_features) == expected_nof_core_features
+
+@pytest.mark.parametrize("model_name, expected_nof_dead_features", [[m[NAME], m[NOF_DEAD_FEATURES]] for m in MODELS])
+def test_nof_dead_features(model_name, expected_nof_dead_features):
+    _, model = get_model(model_name)
+    dead_features = Glucose3DeadFeatures().execute(model).get_result()
+    assert len(dead_features) == expected_nof_dead_features
+
+@pytest.mark.parametrize("model_name, expected_nof_false_optional_features", [[m[NAME], m[NOF_FALSE_OPTIONAL_FEATURES]] for m in MODELS])
+def test_nof_false_optional_features(model_name, expected_nof_false_optional_features):
+    fm, pysat_model = get_model(model_name)
+    optional_features = [f.name for f in fm.get_features() if not f.is_mandatory()]
+    false_optional_features = Glucose3FalseOptionalFeatures(optional_features).execute(pysat_model).get_result()
+    assert len(false_optional_features) == expected_nof_false_optional_features
